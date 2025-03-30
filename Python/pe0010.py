c82065b37abb98011dc52d6c3df6a6bf9faf1e5d
Andrew-Yoo <yoo.andrew08@gmail.com>
2025-03-29 22:40:28 -0400

    Update pe0010.py

--- Python/pe0010.py
+++ Python/pe0010.py
@@ -7,3 +7,23 @@
 # The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 # Find the sum of all the primes below two million.
 # """
+
+def sieve_of_eratosthenes(bound):
+    # Create a boolean array of True values
+    sieve = [True] * bound
+    # 0 and 1 are not prime numbers
+    sieve[0] = sieve[1] = False
+
+    # Iterate through numbers and mark multiples as non-prime
+    for x in range(2, int(bound ** 0.5) + 1):
+        if sieve[x]:  # If the number is still marked as prime
+            for y in range(x ** 2, bound, x):
+                sieve[y] = False
+
+    # Extract all numbers marked as prime
+    primes = [z for z in range(bound) if sieve[z]]
+    return primes
+
+primes = sieve_of_eratosthenes(2000000)
+
+print(sum(primes))
