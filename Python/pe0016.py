cf769cab6d07ab1aa8a79cbb5c3d8c0fd06f95b3
Andrew Yoo <yoo.andrew08@gmail.com>
2025-03-31 04:26:35 -0400

    Update pe0016.py

--- Python/pe0016.py
+++ Python/pe0016.py
@@ -7,3 +7,12 @@
 # 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 # What is the sum of the digits of the number 2^1000?
 # """
+
+def sum_of_digits(number):
+  number = str(number)
+  digits = []
+  for x in range(len(number)):
+    digits.append(int(number[x]))
+  return(sum(digits))
+
+sum_of_digits(2 ** 1000)
