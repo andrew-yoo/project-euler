16bb7aa068ddf9becbedba3a24c73f558381067c
Andrew Yoo <yoo.andrew08@gmail.com>
2025-03-31 03:30:03 -0400

    Update pe0014.py

--- Python/pe0014.py
+++ Python/pe0014.py
@@ -13,3 +13,22 @@
 # Which starting number, under one million, produces the longest chain?
 # NOTE: Once the chain starts the terms are allowed to go above one million.
 # """
+
+def collatz_length(number):
+  length = 1 # Include the first number in the collatz length; doesn't really change the outcome, though
+  while number != 1:
+    if number % 2 == 0:
+      number = number // 2
+    elif number % 2 == 1:
+      number = (3 * number) + 1
+    length += 1
+  return length
+
+longest_chain = 0
+longest_chain_seed = 0
+for x in range(1000000,1,-1): # Running through the loop backwards speeds it up, because the longest chain in a given interval is *usually* near the high end
+  if collatz_length(x) > longest_chain:
+    longest_chain = collatz_length(x)
+    longest_chain_seed = x
+
+print(longest_chain_seed)
