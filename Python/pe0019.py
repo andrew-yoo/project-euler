2ef76d1ba7ccfa39aa5e926be92eb066acfb923a
Andrew Yoo <yoo.andrew08@gmail.com>
2025-04-01 17:58:30 -0400

    Update pe0019.py

--- Python/pe0019.py
+++ Python/pe0019.py
@@ -16,3 +16,14 @@
 # How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
 # """
 
+import datetime as date
+
+def count_first_of_month(start_year, end_year, start_month, end_month, weekday):
+  result = 0
+  for a in range(start_year, end_year + 1):
+    for b in range(start_month, end_month + 1):
+      if date.date(a, b, 1).weekday() == (weekday - 1):
+        result += 1
+  return result
+
+count_first_of_month(1901, 2000, 1, 12, 7)
