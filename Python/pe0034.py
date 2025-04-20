9bea0969ea1d52f5ae1ac2f3c94320294a16561e
Andrew Yoo <yoo.andrew08@gmail.com>
2025-04-20 18:25:16 -0400

    Update pe0034.py

--- Python/pe0034.py
+++ Python/pe0034.py
@@ -11,3 +11,22 @@
 # Note: as 1! = 1 and 2! = 2 are not sums they are not included.
 # """
 
+import math
+
+def check_curious(num):
+  num_digits = []
+  for a in range(len(str(num))):
+    num_digits.append(int(str(num)[a]))
+  
+  truth = 0
+  for item in num_digits:
+    truth += math.factorial(item)
+  return(truth == num)
+
+curious = set()
+
+for x in range(10, 100000): # Can be adjusted
+  if check_curious(x):
+    curious.add(x)
+
+print(sum(curious))
