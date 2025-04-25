4d7bd67ccaabba803d05830ea935b525fde71555
Andrew Yoo <yoo.andrew08@gmail.com>
2025-04-25 17:50:14 -0400

    Update pe0036.py

--- Python/pe0036.py
+++ Python/pe0036.py
@@ -10,3 +10,15 @@
 # 
 # (Please note that the palindromic number, in either base, may not include leading zeros.)
 # """
+
+def base10_to_binary(number):
+    binary = bin(number)[2:]
+    return binary
+
+double_base_palindromes = set()
+  
+for a in range(1_000_000):
+    if list(str(a)) == list(reversed(str(a))) and list(str(base10_to_binary(a)))== list(reversed(str(base10_to_binary(a)))):
+        double_base_palindromes.add(a)
+
+print(sum(double_base_palindromes))
