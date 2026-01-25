bd8879ec15948abe98d94543616602c4d38e193c
Andrew Yoo <199436365+andrew-yoo@users.noreply.github.com>
2026-01-25 16:26:44 -0500

    Finish Python 49

--- Python/pe0049.py
+++ Python/pe0049.py
@@ -14,3 +14,45 @@
 # What 12-digit number do you form by concatenating the three terms in this sequence?
 # """"
 
+from math import sqrt, ceil
+
+def is_prime(n):
+    for i in range(2,ceil(sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    
+    return True
+
+def primes_list(a, b):
+    primes = []
+
+    for i in range(1000, 10000):
+        if is_prime(i):
+            primes.append(i)
+    
+    return primes
+
+def are_permutations(num1, num2):
+    set1 = set(str(num1))
+    set2 = set(str(num2))
+
+    return (set1 == set2) and (len(set1) == len(set2))
+
+def find():
+    primes = primes_list(1_000, 10_000)
+    for a in primes:
+        for i in range(1, ceil((10_000 - a)/2)):
+            b = a + i
+            c = b + i
+
+            if (b in primes) and (c in primes):
+                if (are_permutations(a,b)) and (are_permutations(a,c)):
+                    if a != 1487:
+                        return a, b, c
+
+
+def main():
+    a, b, c = find()
+    print(f'{a}{b}{c}')         
+
+main()
