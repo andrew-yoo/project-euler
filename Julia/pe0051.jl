e68d79bc5036113830290acbef377990f3fd5bf0
Andrew Yoo <199436365+andrew-yoo@users.noreply.github.com>
2026-03-27 17:06:32 -0400

    Python 67

---
+++ Julia/pe0051.jl
@@ -0,0 +1,36 @@
+# Project Euler
+# Prime Digit Replacements
+# Problem 51
+# https://projecteuler.net/problem=51
+#
+# """
+# By replacing the 1st digit of the 2-digit number *3, 
+# it turns out that six of the nine possible values: 13, 23, 43, 53, 73, and 83, are all prime.
+# 
+# By replacing the 3rd and 4th digits of 56**3 with the same digit, 
+# this 5-digit number is the first example having seven primes among the ten generated numbers, 
+# yielding the family: 56003, 56113, 56333, 56443, 56663, 56773, and 56993. 
+# Consequently 56003, being the first member of this family, is the smallest prime with this property.
+# 
+# Find the smallest prime which, by replacing part of the number (not necessarily adjacent digits) with the same digit, 
+# is part of an eight prime value family.
+# """
+
+t1 = time()
+
+using Primes
+
+
+
+function is_prime(upper)
+    
+end
+
+function answer()
+    return
+end
+
+println(answer())
+
+t2 = time()
+println(t2 - t1)
